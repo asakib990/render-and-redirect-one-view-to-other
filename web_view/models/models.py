4c0d294827ff73a967ef7555bcf0f75632dbf12a
SAKIB <68583425+asakib990@users.noreply.github.com>
2023-03-14 15:52:54 +0600

    Delete web_view directory

--- web_view/models/models.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class MyModuleData(models.Model):
-    _name = 'website.data'
-
-    name = fields.Char()
-    description = fields.Text()
-    date = fields.Date()
